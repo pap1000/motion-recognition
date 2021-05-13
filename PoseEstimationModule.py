749d3383d2b71a46afaeb0c9d00b0c5aeee79b9f
pap1000 <74577654+pap1000@users.noreply.github.com>
2021-05-13 18:43:46 +0900

    Update PoseEstimationModule.py
    동작구분 시 필요한 거리, 각도 추가

--- PoseEstimationModule.py
+++ PoseEstimationModule.py
@@ -335,6 +335,166 @@ def pose_estimation(frame, holistic, dict_PoseAngle, dict_PoseLength, white_img)
     except:
         dict_PoseAngle['RA-RK-LA'] = -1
 
+    # LeftWrist-RightWrist-LeftAnkle 각도 구하기, FBM-5
+    try:
+        LeftWrist = [landmarks[mp_pose.PoseLandmark.LEFT_WRIST.value].x,
+                      landmarks[mp_pose.PoseLandmark.LEFT_WRIST.value].y]
+        RightWrist = [landmarks[mp_pose.PoseLandmark.RIGHT_WRIST.value].x,
+                     landmarks[mp_pose.PoseLandmark.RIGHT_WRIST.value].y]
+        LeftAnkle = [landmarks[mp_pose.PoseLandmark.LEFT_ANKLE.value].x,
+                     landmarks[mp_pose.PoseLandmark.LEFT_ANKLE.value].y]
+
+        angle = calculate_angle(LeftWrist, RightWrist, LeftAnkle)
+        cv2.putText(image, str(int(angle)),
+                    tuple(np.multiply(RightWrist, [640, 480]).astype(int)),
+                    cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2, cv2.LINE_AA
+                    )
+
+        dict_PoseAngle['LW-RW-LA'] = angle
+
+    except:
+        dict_PoseAngle['LW-RW-LA'] = -1
+
+    # RightWrist-LeftWrist-RightAnkle 각도 구하기, FBM-5
+    try:
+        RightWrist = [landmarks[mp_pose.PoseLandmark.RIGHT_WRIST.value].x,
+                     landmarks[mp_pose.PoseLandmark.RIGHT_WRIST.value].y]
+        LeftWrist = [landmarks[mp_pose.PoseLandmark.LEFT_WRIST.value].x,
+                      landmarks[mp_pose.PoseLandmark.LEFT_WRIST.value].y]
+        RightAnkle = [landmarks[mp_pose.PoseLandmark.RIGHT_ANKLE.value].x,
+                     landmarks[mp_pose.PoseLandmark.RIGHT_ANKLE.value].y]
+
+        angle = calculate_angle(RightWrist, LeftWrist, RightAnkle)
+        cv2.putText(image, str(int(angle)),
+                    tuple(np.multiply(LeftWrist, [640, 480]).astype(int)),
+                    cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2, cv2.LINE_AA
+                    )
+
+        dict_PoseAngle['RW-LW-RA'] = angle
+
+    except:
+        dict_PoseAngle['RW-LW-RA'] = -1
+
+    # LeftWrist-RightAnkle-LeftAnkle 각도 구하기, FBM-5
+    try:
+        LeftWrist = [landmarks[mp_pose.PoseLandmark.LEFT_WRIST.value].x,
+                      landmarks[mp_pose.PoseLandmark.LEFT_WRIST.value].y]
+        RightAnkle = [landmarks[mp_pose.PoseLandmark.RIGHT_ANKLE.value].x,
+                     landmarks[mp_pose.PoseLandmark.RIGHT_ANKLE.value].y]
+        LeftAnkle = [landmarks[mp_pose.PoseLandmark.LEFT_ANKLE.value].x,
+                     landmarks[mp_pose.PoseLandmark.LEFT_ANKLE.value].y]
+
+        angle = calculate_angle(LeftWrist, RightAnkle, LeftAnkle)
+        cv2.putText(image, str(int(angle)),
+                    tuple(np.multiply(RightAnkle, [640, 480]).astype(int)),
+                    cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2, cv2.LINE_AA
+                    )
+
+        dict_PoseAngle['LW-RA-LA'] = angle
+
+    except:
+        dict_PoseAngle['LW-RA-LA'] = -1
+
+    # RightWrist-LeftWrist-RightAnkle 각도 구하기, FBM-5
+    try:
+        RightWrist = [landmarks[mp_pose.PoseLandmark.RIGHT_WRIST.value].x,
+                     landmarks[mp_pose.PoseLandmark.RIGHT_WRIST.value].y]
+        LeftAnkle = [landmarks[mp_pose.PoseLandmark.LEFT_ANKLE.value].x,
+                      landmarks[mp_pose.PoseLandmark.LEFT_ANKLE.value].y]
+        RightAnkle = [landmarks[mp_pose.PoseLandmark.RIGHT_ANKLE.value].x,
+                     landmarks[mp_pose.PoseLandmark.RIGHT_ANKLE.value].y]
+
+        angle = calculate_angle(RightWrist, LeftAnkle, RightAnkle)
+        cv2.putText(image, str(int(angle)),
+                    tuple(np.multiply(LeftAnkle, [640, 480]).astype(int)),
+                    cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2, cv2.LINE_AA
+                    )
+
+        dict_PoseAngle['RW-LA-RA'] = angle
+
+    except:
+        dict_PoseAngle['RW-LA-RA'] = -1
+
+    # RightWrist-LeftWrist-LeftAnkle 각도 구하기, FBM-6
+    try:
+        RightWrist = [landmarks[mp_pose.PoseLandmark.RIGHT_WRIST.value].x,
+                     landmarks[mp_pose.PoseLandmark.RIGHT_WRIST.value].y]
+        LeftWrist = [landmarks[mp_pose.PoseLandmark.LEFT_WRIST.value].x,
+                      landmarks[mp_pose.PoseLandmark.LEFT_WRIST.value].y]
+        LeftAnkle = [landmarks[mp_pose.PoseLandmark.LEFT_ANKLE.value].x,
+                     landmarks[mp_pose.PoseLandmark.LEFT_ANKLE.value].y]
+
+        angle = calculate_angle(RightWrist, LeftWrist, LeftAnkle)
+        cv2.putText(image, str(int(angle)),
+                    tuple(np.multiply(LeftWrist, [640, 480]).astype(int)),
+                    cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2, cv2.LINE_AA
+                    )
+
+        dict_PoseAngle['RW-LW-LA'] = angle
+
+    except:
+        dict_PoseAngle['RW-LW-LA'] = -1
+
+    # LeftWrist-RightWrist-RightAnkle 각도 구하기, FBM-6
+    try:
+        LeftWrist = [landmarks[mp_pose.PoseLandmark.LEFT_WRIST.value].x,
+                     landmarks[mp_pose.PoseLandmark.LEFT_WRIST.value].y]
+        RightWrist = [landmarks[mp_pose.PoseLandmark.RIGHT_WRIST.value].x,
+                      landmarks[mp_pose.PoseLandmark.RIGHT_WRIST.value].y]
+        RightAnkle = [landmarks[mp_pose.PoseLandmark.RIGHT_ANKLE.value].x,
+                     landmarks[mp_pose.PoseLandmark.RIGHT_ANKLE.value].y]
+
+        angle = calculate_angle(LeftWrist, RightWrist, RightAnkle)
+        cv2.putText(image, str(int(angle)),
+                    tuple(np.multiply(RightWrist, [640, 480]).astype(int)),
+                    cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2, cv2.LINE_AA
+                    )
+
+        dict_PoseAngle['LW-RW-RA'] = angle
+
+    except:
+        dict_PoseAngle['LW-RW-RA'] = -1
+
+    # RightWrist-RightAnkle-LeftAnkle 각도 구하기, FBM-6
+    try:
+        RightWrist = [landmarks[mp_pose.PoseLandmark.RIGHT_WRIST.value].x,
+                     landmarks[mp_pose.PoseLandmark.RIGHT_WRIST.value].y]
+        RightAnkle = [landmarks[mp_pose.PoseLandmark.RIGHT_ANKLE.value].x,
+                      landmarks[mp_pose.PoseLandmark.RIGHT_ANKLE.value].y]
+        LeftAnkle = [landmarks[mp_pose.PoseLandmark.LEFT_ANKLE.value].x,
+                     landmarks[mp_pose.PoseLandmark.LEFT_ANKLE.value].y]
+
+        angle = calculate_angle(RightWrist, RightAnkle, LeftAnkle)
+        cv2.putText(image, str(int(angle)),
+                    tuple(np.multiply(RightAnkle, [640, 480]).astype(int)),
+                    cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2, cv2.LINE_AA
+                    )
+
+        dict_PoseAngle['RW-RA-LA'] = angle
+
+    except:
+        dict_PoseAngle['RW-RA-LA'] = -1
+
+    # LeftWrist-RightAnkle-RightAnkle 각도 구하기, FBM-6
+    try:
+        LeftWrist = [landmarks[mp_pose.PoseLandmark.LEFT_WRIST.value].x,
+                     landmarks[mp_pose.PoseLandmark.LEFT_WRIST.value].y]
+        LeftAnkle = [landmarks[mp_pose.PoseLandmark.LEFT_ANKLE.value].x,
+                      landmarks[mp_pose.PoseLandmark.LEFT_ANKLE.value].y]
+        RightAnkle = [landmarks[mp_pose.PoseLandmark.RIGHT_ANKLE.value].x,
+                     landmarks[mp_pose.PoseLandmark.RIGHT_ANKLE.value].y]
+
+        angle = calculate_angle(LeftWrist, LeftAnkle, RightAnkle)
+        cv2.putText(image, str(int(angle)),
+                    tuple(np.multiply(LeftAnkle, [640, 480]).astype(int)),
+                    cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2, cv2.LINE_AA
+                    )
+
+        dict_PoseAngle['LW-LA-RA'] = angle
+
+    except:
+        dict_PoseAngle['LW-LA-RA'] = -1
+
 
     # 16번과 14번 사이 거리 구하기
     try:
@@ -667,5 +827,82 @@ def pose_estimation(frame, holistic, dict_PoseAngle, dict_PoseLength, white_img)
 
     except:
         dict_PoseLength['28, 27'] = -1
+
+    # 26번과 16번 사이 거리 구하기
+    try:
+        RightKnee = [landmarks[mp_pose.PoseLandmark.RIGHT_KNEE.value].x,
+                     landmarks[mp_pose.PoseLandmark.RIGHT_KNEE.value].y]
+        RightWrist = [landmarks[mp_pose.PoseLandmark.RIGHT_WRIST.value].x,
+                      landmarks[mp_pose.PoseLandmark.RIGHT_WRIST.value].y]
+
+
+        length = math.hypot(RightKnee[0] - RightWrist[0], RightKnee[1] - RightWrist[1]) * 100
+        cv2.putText(image, str(length),
+                    tuple(np.multiply(RightKnee, [640, 500]).astype(int)),
+                    cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 1, cv2.LINE_AA
+                    )
+
+        dict_PoseLength['26, 16'] = length
+
+    except:
+        dict_PoseLength['26, 16'] = -1
+
+    # 26번과 15번 사이 거리 구하기
+    try:
+        RightKnee = [landmarks[mp_pose.PoseLandmark.RIGHT_KNEE.value].x,
+                     landmarks[mp_pose.PoseLandmark.RIGHT_KNEE.value].y]
+        LeftWrist = [landmarks[mp_pose.PoseLandmark.LEFT_WRIST.value].x,
+                      landmarks[mp_pose.PoseLandmark.LEFT_WRIST.value].y]
+
+
+        length = math.hypot(RightKnee[0] - LeftWrist[0], RightKnee[1] - LeftWrist[1]) * 100
+        cv2.putText(image, str(length),
+                    tuple(np.multiply(RightKnee, [640, 500]).astype(int)),
+                    cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 1, cv2.LINE_AA
+                    )
+
+        dict_PoseLength['26, 15'] = length
+
+    except:
+        dict_PoseLength['26, 15'] = -1
+
+    # 25번과 16번 사이 거리 구하기
+    try:
+        LeftKnee = [landmarks[mp_pose.PoseLandmark.LEFT_KNEE.value].x,
+                     landmarks[mp_pose.PoseLandmark.LEFT_KNEE.value].y]
+        LeftWrist = [landmarks[mp_pose.PoseLandmark.LEFT_WRIST.value].x,
+                      landmarks[mp_pose.PoseLandmark.LEFT_WRIST.value].y]
+
+
+        length = math.hypot(LeftKnee[0] - LeftWrist[0], LeftKnee[1] - LeftWrist[1]) * 100
+        cv2.putText(image, str(length),
+                    tuple(np.multiply(LeftKnee, [640, 500]).astype(int)),
+                    cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 1, cv2.LINE_AA
+                    )
+
+        dict_PoseLength['25, 16'] = length
+
+    except:
+        dict_PoseLength['25, 16'] = -1
+
+    # 25번과 15번 사이 거리 구하기
+    try:
+        LeftKnee = [landmarks[mp_pose.PoseLandmark.LEFT_KNEE.value].x,
+                     landmarks[mp_pose.PoseLandmark.LEFT_KNEE.value].y]
+        RightWrist = [landmarks[mp_pose.PoseLandmark.RIGHT_WRIST.value].x,
+                      landmarks[mp_pose.PoseLandmark.RIGHT_WRIST.value].y]
+
+
+        length = math.hypot(LeftKnee[0] - RightWrist[0], LeftKnee[1] - RightWrist[1]) * 100
+        cv2.putText(image, str(length),
+                    tuple(np.multiply(LeftKnee, [640, 500]).astype(int)),
+                    cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 1, cv2.LINE_AA
+                    )
+
+        dict_PoseLength['25, 15'] = length
+
+    except:
+        dict_PoseLength['25, 15'] = -1
+
 
     return image, white_img
